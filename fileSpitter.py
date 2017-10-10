d97b37ec917a8e2212653783a11bd354967d606f
Evan Genest <genest.1@osu.edu>
2017-10-10 12:41:42 -0500

    Make program open any file name that was entered at the command line.  Then simply close the file.

--- fileSpitter.py
+++ fileSpitter.py
@@ -3,20 +3,27 @@
 # Utility to remove lines
 # Useage:  enter PYTHON3 FILESPITTER.PY <f8oehaje> <word that identifies which lines to keep.>
 
-# COMMAND LINE
+
+import sys 	# Allows argv? Definitely allows sys.exit
+
 if len(sys.argv) < 3:
-	print('Enter two command line options')
+	print('Enter two command line options.')
+	print('Usage: PYTHON3 FILESPITTER <FILENAME> <TERM PRESENT IF LINE IS TO SURVIVE>')
 	sys.exit()
 print('User chose ' , sys.argv[1] + ' option')
-if ( sys.argv[1].isnumeric() == 0  ):
-	print('Useage: baseball.py player wanted , stat wanted (starting from zero lowest)')
-	sys.exit()
+
+print('You said ' + sys.argv[0])
+print('You said ' + sys.argv[1])
+print('You said ' + sys.argv[2])
+print('Is that correct?')
 
 # READ FILE INTO A COMPLEX AND LARGE LEAGUE-WIDE STRUCTURE IN MEMORY
+fout = open ('a.txt', 'w')
 try:
-	fp = open( '/home/atom_box/Dropbox/code/data/tigers2016.csv', 'r')
+	fin = open( sys.argv[1], 'r')
 except:
 	print('Whoopsies...no file found!')
 	sys.exit()
-
-print ('Succesfully opened ', fp.name )
+fout.close()
+fin.close()
+print ('Succesfully opened ', fin.name )
