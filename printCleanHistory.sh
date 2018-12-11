4e2bd4c127cf38c9f7cec23f4d2fcfd66a66c18b
Evan Genest <genest.1@osu.edu>
2018-12-11 08:03:53 -0600

    Add comments. Add pipes.

--- printCleanHistory.sh
+++ printCleanHistory.sh
@@ -1,1 +1,13 @@
- sed 's_......__' history2000.txt | sed 's_sudo.*__' | sed 's_ssh.*__' | sed 's_.*ftp.*__' | sort | uniq
+ sed 's_......__' myHistory.txt |
+ sed 's_sudo.*__' |
+ sed 's_ssh.*__'  |
+ sed 's_.*ftp.*__' |
+ sort |
+ uniq
+
+#########
+# Purpose: Redact personal data from a BASH history listing
+# Useage: 
+# $ history > myHistory.txt
+# $ ./printCleanHistory.sh > outfile.txt
+#########
