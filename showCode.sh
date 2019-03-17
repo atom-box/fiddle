e92a2cb5be0a69878a8972b763556764d9aab175
Evan Genest <genest.1@osu.edu>
2019-03-17 07:09:00 -0500

    Add reuseable syntax: BASH sanity check.

--- showCode.sh
+++ showCode.sh
@@ -1,11 +1,20 @@
 
 #!/bin/bash
-if [ -z "$1" ]; then
-	echo Syntax for this is: $0 myfile.txt lineStart lineStop
+if (($# < 3)); then
+	echo
+	echo ----UNCOOL SYNTAX -----
+	echo You gave $# arguments.  Function expects three arguments.
+	echo ----YOU GIVE ME-----
+	echo Better syntax would be similar to: $0 myfile.HTML! lineStart lineStop
+	echo ----I GIVE YOU -----
+	echo Your requested lines of html will have all problematic angle brackets replaced with a displayable symbol.
+	echo
 	exit
 fi
 
-head -n $3 $1 | tail -n ($3 - $2) | sed 's/</\&lt/g' | sed 's/>/\&gt/g'
+# Syntax to remember: DOUBLE parens, no dollar sign until dereferencing
+((SPAN = $3 - $2))
+# Syntax you never knew $# is the length of args array   --devnotes
+head -n $3 $1 | tail -n $SPAN | sed 's/</\&lt/g' | sed 's/>/\&gt/g'
 
-#  finish this script (look up the BASH subtraction syntax)
 # go to alice server and make things nice write actual content TODAY
