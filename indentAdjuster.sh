793bf1c6e44dabea9f71c98a186a1b9dedc93015
Evan Genest <genest.1@osu.edu>
2019-10-08 17:31:39 -0500

    Script to change spaces of indentation.

---
+++ indentAdjuster.sh
@@ -0,0 +1,12 @@
+# USEAGE: ./indentAdjuster.sh infilename(string) factor(integer)
+
+# This script will adjust the indenting of SPACE indented files.  If tabbed, resave your files as spaced before using this script.  To save to an outfile, use UNIX redirect at the CLI.
+# FACTOR is an integer by which to multiply (+) or divide(-) the current spacing on indents
+#  @MISTERGENEST
+
+#!/bin/bash
+echo ":::::::::::"
+date
+
+# PRINT READOUT OF LEADING SPACES, rest OF LINE REMOVED
+sed $1 
