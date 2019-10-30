ba2af5b030fc2b59746fffe93ff75ecfe6ba1773
Evan Genest <genest.1@osu.edu>
2019-10-30 06:36:40 -0500

    Add a useage note and comments.

--- wordListifier.sh
+++ wordListifier.sh
@@ -1,7 +1,17 @@
-#script to remove capitalized words
-# and nonalpha
+# This script will read a text file,
+# strip its punctuation and
+# proper nouns (if capitalized)
+# and empty lines.
+# It will output all of the words,
+# and append NEWLINE after each word.
+# I use it as a preprocessor before
+# https://www.wordclouds.com/ 
 
-
+if [ -z "$1" ]; then
+	echo You must specify a file name. 
+	echo Please type a filename as a command line argument.
+	exit
+fi
 
 
 sed 's/[^A-z]/+/g' $1 |
